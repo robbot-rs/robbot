7a65b774a8672a09e5633b6e2062043173c9c488
MrGunflame <mrgunflame@protonmail.com>
2021-10-10 03:55:46 +0200

    Add more context aliases

--- src/bot.rs
+++ src/bot.rs
@@ -1,5 +1,10 @@
 use crate::core::state::State;
-use serenity::model::channel::Message;
+use serenity::model::{
+    channel::{GuildChannel, Message, Reaction},
+    guild::Member,
+    id::{ChannelId, GuildId, MessageId},
+    user::User,
+};
 use std::convert::From;
 use std::error;
 use std::sync::Arc;
@@ -27,7 +32,16 @@ where
 pub type Result = std::result::Result<(), Error>;
 
 // Context aliases.
+pub type ChannelCreateContext = Context<GuildChannel>;
+pub type ChannelDeleteContext = Context<GuildChannel>;
+pub type GuildMemberAdditionContext = Context<(GuildId, Member)>;
+pub type GuildmemberRemovalContext = Context<(GuildId, User, Option<Member>)>;
+pub type GuildMemberUpdateContext = Context<(Option<Member>, Member)>;
 pub type MessageContext = Context<Message>;
+pub type ReactionAddContext = Context<Reaction>;
+pub type ReactionRemoveContext = Context<Reaction>;
+pub type ReactionRemoveAllContext = Context<(ChannelId, MessageId)>;
+
 pub type TaskContext = Context<()>;
 
 #[derive(Clone)]
@@ -36,6 +50,17 @@ pub struct Context<T> {
     pub state: Arc<State>,
     pub args: Vec<String>,
     pub event: T,
+}
+
+impl<T> Context<T> {
+    pub fn new(raw_ctx: serenity::client::Context, state: Arc<State>, event: T) -> Self {
+        Self {
+            raw_ctx,
+            state,
+            args: Vec::new(),
+            event,
+        }
+    }
 }
 
 // impl Context<Message> {
