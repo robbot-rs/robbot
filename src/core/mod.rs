19ed9070e9abbf1781baa0ab5573cd1cecbb1a64
MrGunflame <mrgunflame@protonmail.com>
2021-10-20 18:29:49 +0200

    Remove src (moved into robbot-bin)

--- src/core/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod command;
-pub mod executor;
-pub mod hook;
-pub mod module;
-pub mod router;
-pub mod state;
-pub mod store;
-pub mod task;
--- src/plugins/mod.rs
+++
@@ -1,2 +0,0 @@
-#[cfg(feature = "debug")]
-pub mod debug;
