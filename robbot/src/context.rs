9939673615e1ce1dcd7db0022c8bea50ec3ff4c4
MrGunflame <mrgunflame@protonmail.com>
2021-10-27 23:48:18 +0200

    Add create_reaction and react to Context

--- robbot/src/context.rs
+++ robbot/src/context.rs
@@ -1,11 +1,15 @@
 use crate::{builder::CreateMessage, model::Message};
 use async_trait::async_trait;
-use serenity::model::id::{ChannelId, MessageId};
+use serenity::model::{
+    channel::ReactionType,
+    id::{ChannelId, MessageId},
+};
 
 #[async_trait]
-pub trait Context: Sync {
+pub trait Context {
     type Error;
 
+    /// Create a new message a text channel.
     async fn send_message<T>(
         &self,
         channel_id: ChannelId,
@@ -13,6 +17,16 @@ pub trait Context: Sync {
     ) -> Result<Message, Self::Error>
     where
         T: Into<CreateMessage> + Send + Sync;
+
+    /// Create a new reaction on a message.
+    async fn create_reaction<T>(
+        &self,
+        message_id: MessageId,
+        channel_id: ChannelId,
+        reaction: T,
+    ) -> Result<(), Self::Error>
+    where
+        T: Into<ReactionType> + Send;
 
     /// Respond to the message author. Returns the newly created message.
     async fn respond<T>(&self, message: T) -> Result<Message, Self::Error>
@@ -27,5 +41,18 @@ pub trait Context: Sync {
         message.reference_message((channel_id, message_id));
 
         self.send_message(channel_id, message).await
+    }
+
+    /// React to the message the author sent.
+    async fn react<T>(&self, reaction: T) -> Result<(), Self::Error>
+    where
+        Self: AsRef<MessageId> + AsRef<ChannelId>,
+        T: Into<ReactionType> + Send,
+    {
+        let message_id: MessageId = *self.as_ref();
+        let channel_id: ChannelId = *self.as_ref();
+
+        self.create_reaction(message_id, channel_id, reaction.into())
+            .await
     }
 }
