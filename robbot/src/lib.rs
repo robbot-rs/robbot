d07feaae2fb5688f4a6e5080fc332c3167c1472b
MrGunflame <mrgunflame@protonmail.com>
2021-10-31 03:10:44 +0100

    Add prelude export

--- robbot/src/lib.rs
+++ robbot/src/lib.rs
@@ -10,7 +10,21 @@ pub mod task;
 
 pub use robbot_derive::command;
 pub use {
+    arguments::Arguments,
+    bot::{Error, Result},
     command::Command,
     context::Context,
     task::{Task, TaskSchedule},
 };
+
+pub mod prelude {
+    pub use crate::{
+        bot::{
+            Error::{self, InvalidCommandUsage},
+            Result,
+        },
+        command::Command,
+        context::Context,
+    };
+    pub use robbot_derive::command;
+}
