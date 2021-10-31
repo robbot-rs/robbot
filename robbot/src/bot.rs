af41a7c6e07f04b495eb96ec5c0bf5928f76e657
MrGunflame <mrgunflame@protonmail.com>
2021-10-31 03:11:02 +0100

    Impl Display for Error

--- robbot/src/bot.rs
+++ robbot/src/bot.rs
@@ -1,4 +1,8 @@
-use std::{error, result};
+use std::{
+    error,
+    fmt::{self, Display, Formatter},
+    result,
+};
 
 pub type Result = result::Result<(), Error>;
 
@@ -7,9 +11,21 @@ pub enum Error {
     InvalidCommandUsage,
     Unimplemented,
     NoResponse,
-    BoxError(Box<dyn error::Error + Send + Sync + 'static>),
     /// Hook collector timed out.
     HookTimeout,
+    BoxError(Box<dyn error::Error + Send + Sync + 'static>),
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        match self {
+            Self::InvalidCommandUsage => write!(f, "invalid command usage"),
+            Self::Unimplemented => write!(f, "unimplemented"),
+            Self::NoResponse => write!(f, "no response"),
+            Self::HookTimeout => write!(f, "hook timeout"),
+            Self::BoxError(err) => err.fmt(f),
+        }
+    }
 }
 
 impl<T> From<T> for Error
