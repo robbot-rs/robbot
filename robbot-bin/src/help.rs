8448b63e623387a85d2b7955422365929705bb43
MrGunflame <mrgunflame@protonmail.com>
2021-11-15 13:36:27 +0100

    Generalize global and command help fns to accept any Command type

--- robbot-bin/src/help.rs
+++ robbot-bin/src/help.rs
@@ -1,34 +1,38 @@
-use {
-    crate::core::command::Command,
-    std::{collections::HashSet, fmt::Write},
-};
+use robbot::command::Command;
+use std::{collections::HashSet, fmt::Write};
 
-pub(crate) fn global(commands: &HashSet<Command>) -> String {
+pub(crate) fn global<T>(commands: &HashSet<T>) -> String
+where
+    T: Command,
+{
     let mut string = String::new();
     for command in commands {
-        writeln!(string, "- {}", command.name).unwrap();
+        writeln!(string, "- {}", command.name()).unwrap();
     }
 
     string
 }
 
 /// Return Command help
-pub(crate) fn command(command: &Command) -> String {
+pub(crate) fn command<T>(command: &T) -> String
+where
+    T: Command,
+{
     let mut string = String::new();
 
-    let _ = writeln!(string, "**Name**: {}", command.name);
-    let _ = writeln!(string, "**Description**: {}", command.description);
+    let _ = writeln!(string, "**Name**: {}", command.name());
+    let _ = writeln!(string, "**Description**: {}", command.description());
 
-    if command.executor.is_some() {
-        let _ = writeln!(string, "**Usage**: {}", command.usage);
-        let _ = writeln!(string, "**Example**: {}", command.example);
+    if command.executor().is_some() {
+        let _ = writeln!(string, "**Usage**: {}", command.usage());
+        let _ = writeln!(string, "**Example**: {}", command.example());
     }
 
-    if !command.sub_commands.is_empty() {
+    if !command.sub_commands().is_empty() {
         writeln!(string, "**Sub Commands**:").unwrap();
 
-        for command in &command.sub_commands {
-            writeln!(string, "- {}", command.name).unwrap();
+        for command in command.sub_commands() {
+            writeln!(string, "- {}", command.name()).unwrap();
         }
     }
 
