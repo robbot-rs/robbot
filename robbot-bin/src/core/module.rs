43e6dde196e5788476ca592fc0432239552ceceb
MrGunflame <mrgunflame@protonmail.com>
2021-11-15 13:36:00 +0100

    Add Module

--- robbot-bin/src/core/module.rs
+++ robbot-bin/src/core/module.rs
@@ -1,45 +1,59 @@
 //!
-// use crate::core::{command::Command, task::Task};
-// use std::{
-//     borrow::Borrow,
-//     collections::HashSet,
-//     hash::{Hash, Hasher},
-// };
+use crate::core::{command::Command, task::Task};
+use std::{
+    borrow::Borrow,
+    collections::HashSet,
+    hash::{Hash, Hasher},
+    sync::atomic::{AtomicU64, Ordering},
+};
 
-// // /// A `Module` is a collection of [`Command`]s, [`Task`]s and
-// // /// [`Hook`]s organised under a unique name.
-// pub struct Module {
-//     /// Name and unique identifier for the module. Loading
-//     /// a second module with the same name will fail.
-//     pub name: String,
-//     /// A list of commands associated with a module. When
-//     /// loading a module all commands will be loaded. If
-//     /// a single command fails to register the module fails
-//     /// to load.
-//     pub commands: Option<HashSet<Command>>,
-//     /// A list of Tasks associated with a module.
-//     pub tasks: Option<Vec<Task>>,
-// }
+static HANDLE_SEQ: AtomicU64 = AtomicU64::new(0);
 
-// impl Hash for Module {
-//     fn hash<H>(&self, state: &mut H)
-//     where
-//         H: Hasher,
-//     {
-//         self.name.hash(state);
-//     }
-// }
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+#[repr(transparent)]
+pub struct ModuleHandle(u64);
 
-// impl PartialEq for Module {
-//     fn eq(&self, other: &Self) -> bool {
-//         self.name == other.name
-//     }
-// }
+impl ModuleHandle {
+    pub(crate) fn new() -> Self {
+        Self(HANDLE_SEQ.fetch_add(1, Ordering::SeqCst))
+    }
+}
 
-// impl Eq for Module {}
+// /// A `Module` is a collection of [`Command`]s, [`Task`]s and
+// /// [`Hook`]s organised under a unique name.
+#[derive(Clone)]
+pub struct Module {
+    /// Name and unique identifier for the module. Loading
+    /// a second module with the same name will fail.
+    pub name: String,
+    /// A list of commands associated with a module. When
+    /// loading a module all commands will be loaded. If
+    /// a single command fails to register the module fails
+    /// to load.
+    pub commands: Option<HashSet<Command>>,
+    /// A list of Tasks associated with a module.
+    pub tasks: Option<Vec<Task>>,
+}
 
-// impl Borrow<str> for Module {
-//     fn borrow(&self) -> &str {
-//         &self.name
-//     }
-// }
+impl Hash for Module {
+    fn hash<H>(&self, state: &mut H)
+    where
+        H: Hasher,
+    {
+        self.name.hash(state);
+    }
+}
+
+impl PartialEq for Module {
+    fn eq(&self, other: &Self) -> bool {
+        self.name == other.name
+    }
+}
+
+impl Eq for Module {}
+
+impl Borrow<str> for Module {
+    fn borrow(&self) -> &str {
+        &self.name
+    }
+}
