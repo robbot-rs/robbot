dcca1a83013f2a5ba23523ce540537a074d9eb7d
MrGunflame <mrgunflame@protonmail.com>
2021-11-15 13:28:27 +0100

    Change Command type into trait

--- robbot/src/command.rs
+++ robbot/src/command.rs
@@ -1,89 +1,25 @@
-use serde::{Deserialize, Serialize};
-use std::{
-    borrow::Borrow,
-    collections::HashSet,
-    hash::{Hash, Hasher},
-};
+use std::{borrow::Borrow, collections::HashSet, hash::Hash};
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct Command {
-    pub name: String,
-    pub description: String,
-    /// Defines the usage field in the help message.
-    /// Required arguments should be enclosed by `[]`,
-    /// optional arguments should be enclosed by `<>`.
-    pub usage: String,
-    pub example_usage: String,
-    pub guild_only: bool,
-    pub sub_commands: HashSet<Self>,
-}
+pub trait Command: Sized + Hash + Eq + Borrow<str> {
+    type Executor;
 
-impl Command {
-    /// Create a new [`Command`] with a name and defaulted
-    /// fields.
-    pub fn new<T>(name: T) -> Self
-    where
-        T: ToString,
-    {
-        Self {
-            name: name.to_string(),
-            description: String::new(),
-            usage: String::new(),
-            example_usage: String::new(),
-            guild_only: false,
-            sub_commands: HashSet::new(),
-        }
-    }
-
-    /// Set the `description` field of the command.
-    pub fn description<T>(&mut self, description: T)
-    where
-        T: ToString,
-    {
-        self.description = description.to_string();
-    }
-
-    /// Set the `usage` field of the command.
-    pub fn usage<T>(&mut self, usage: T)
-    where
-        T: ToString,
-    {
-        self.usage = usage.to_string();
-    }
-
-    /// Set the `example_usage` field of the command.
-    pub fn example_usage<T>(&mut self, example_usage: T)
-    where
-        T: ToString,
-    {
-        self.example_usage = example_usage.to_string();
-    }
-
-    /// Set the `guild_only` field of the command.
-    pub fn guild_only<T>(&mut self, guild_only: bool) {
-        self.guild_only = guild_only;
-    }
-}
-
-impl Hash for Command {
-    fn hash<H>(&self, state: &mut H)
-    where
-        H: Hasher,
-    {
-        self.name.hash(state);
-    }
-}
-
-impl PartialEq for Command {
-    fn eq(&self, other: &Self) -> bool {
-        self.name == other.name
-    }
-}
-
-impl Eq for Command {}
-
-impl Borrow<str> for Command {
-    fn borrow(&self) -> &str {
-        &self.name
-    }
+    fn name(&self) -> &str;
+    /// Description of the command. Shown to the user in
+    /// the help command.
+    fn description(&self) -> &str;
+    /// Usage of the command. Shown to the user in the help
+    /// command. Use `<>` for required arguments and `[]` for
+    /// optional commands. Only write the arguments to the usage
+    /// field, **do not** write the full command path.
+    /// Example: `<User> [Message]`
+    fn usage(&self) -> &str;
+    /// Example usage shown to the user in the help command.
+    /// The example should match defined usage.
+    fn example(&self) -> &str;
+    /// Whether the command should only be usable inside
+    /// guilds. Note that if the command is guild-only all
+    /// subcommands will infer the guild-only property.
+    fn guild_only(&self) -> bool;
+    fn sub_commands(&self) -> &HashSet<Self>;
+    fn executor(&self) -> Option<&Self::Executor>;
 }
