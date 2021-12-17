0dd1bbcdeadc2ea27ae96e2c1ad3520064b9fea6
MrGunflame <mrgunflame@protonmail.com>
2021-12-17 23:05:26 +0100

    Update parse_args to use OwnedArguments, update find_command to use ArgumentsExt

--- robbot-core/src/router.rs
+++ robbot-core/src/router.rs
@@ -1,20 +1,28 @@
-use robbot::command::Command;
+use robbot::{
+    arguments::{ArgumentsExt, OwnedArguments},
+    command::Command,
+};
 use std::collections::HashSet;
 
-pub fn parse_args(input: &str) -> Vec<&str> {
-    let mut args = Vec::new();
+pub fn parse_args<T>(input: T) -> OwnedArguments
+where
+    T: AsRef<str>,
+{
+    let input = input.as_ref();
+
+    let mut args = OwnedArguments::new();
 
     let mut start = 0;
     let mut esc = false;
     for (i, b) in input.bytes().enumerate() {
         match b {
             b if b == b' ' && !esc => {
-                args.push(&input[start..i]);
+                args.push(input[start..i].to_string());
                 start = i + 1;
             }
             b'"' => {
                 if esc {
-                    args.push(&input[start + 1..i]);
+                    args.push(input[start + 1..i].to_string());
                     start = i + 1;
                 }
                 esc = !esc;
@@ -23,28 +31,26 @@ pub fn parse_args(input: &str) -> Vec<&str> {
         }
     }
 
-    args.push(&input[start..]);
+    args.push(input[start..].to_string());
 
-    args.into_iter().filter(|arg| !arg.is_empty()).collect()
+    args.iter().filter(|arg| !arg.is_empty()).collect()
 }
 
-pub fn find_command<'life0, T>(
-    commands: &'life0 HashSet<T>,
-    args: &mut Vec<&str>,
-) -> Option<&'life0 T>
+pub fn find_command<'life0, T, U>(commands: &'life0 HashSet<T>, args: &mut U) -> Option<&'life0 T>
 where
     T: Command,
+    U: ArgumentsExt,
 {
     if args.is_empty() {
         return None;
     }
 
-    let mut command = commands.get(args.remove(0))?;
+    let mut command = commands.get(args.pop().unwrap())?;
 
-    while let Some(arg) = args.get(0) {
-        match command.sub_commands().get(*arg) {
+    while let Some(arg) = args.pop() {
+        match command.sub_commands().get(arg) {
             Some(cmd) => {
-                args.remove(0);
+                args.pop();
                 command = cmd;
             }
             None => break,
