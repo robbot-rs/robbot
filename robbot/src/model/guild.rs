39e835c234401c8df0f382d1f0abce3bcec31496
MrGunflame <mrgunflame@protonmail.com>
2022-09-11 14:12:40 +0200

    Add Guild type

--- robbot/src/model/guild.rs
+++ robbot/src/model/guild.rs
@@ -2,10 +2,24 @@ use crate as robbot;
 use crate::{Decode, Encode};
 
 use super::id::{GuildId, RoleId};
+use super::permissions::Permissions;
 use super::user::User;
 
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Serialize, Deserialize, Encode, Decode)]
+pub struct Member {
+    pub deaf: bool,
+    pub guild_id: GuildId,
+    pub joined_at: Option<DateTime<Utc>>,
+    pub mute: bool,
+    pub nick: Option<String>,
+    pub roles: Vec<RoleId>,
+    pub user: User,
+    pub pending: bool,
+    pub avatar: Option<String>,
+}
 
 #[derive(Clone, Debug, Serialize, Deserialize, Encode, Decode)]
 pub struct PartialMember {
@@ -17,4 +31,20 @@ pub struct PartialMember {
     pub pending: bool,
     pub guild_id: Option<GuildId>,
     pub user: Option<User>,
+}
+
+impl From<serenity::model::guild::Member> for Member {
+    fn from(src: serenity::model::guild::Member) -> Self {
+        Self {
+            deaf: src.deaf,
+            guild_id: src.guild_id.into(),
+            joined_at: src.joined_at,
+            mute: src.mute,
+            nick: src.nick,
+            roles: src.roles.into_iter().map(|r| r.into()).collect(),
+            user: src.user.into(),
+            pending: src.pending,
+            avatar: src.avatar,
+        }
+    }
 }
