6f0789678067119348a4a3a3fb87649e0d520fa0
MrGunflame <mrgunflame@protonmail.com>
2022-09-11 14:14:43 +0200

    Use correct RoleId in builder

--- robbot/src/model/mod.rs
+++ robbot/src/model/mod.rs
@@ -8,3 +8,16 @@ mod channel_impl;
 mod guild_impl;
 mod permissions_impl;
 mod user_impl;
+
+use thiserror::Error;
+
+/// An error indicating the model data exists in a state that is not considered valid. One should
+/// always prefer using this error instead of panicking.
+///
+/// This would for example be an [`Message`] with the `member` field being `Some(..)` and
+/// the `guild_id` being `None`.
+///
+/// [`Message`]: channel::Message
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Error)]
+#[error("invalid model data")]
+pub struct InvalidModelData;
