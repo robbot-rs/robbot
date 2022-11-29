d383c87ed2cd9aee7ce8c81c1d8221b112f0bc8b
MrGunflame <mrgunflame@protonmail.com>
2022-11-29 13:26:51 +0100

    Add Color::from_rgb

--- robbot/src/util/color.rs
+++ robbot/src/util/color.rs
@@ -6,6 +6,12 @@ use serde::{Deserialize, Serialize};
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize, Encode, Decode)]
 pub struct Color(pub u32);
 
+impl Color {
+    pub const fn from_rgb(red: u8, green: u8, blue: u8) -> Self {
+        Self((red as u32) << 16 | (green as u32) << 8 | blue as u32)
+    }
+}
+
 impl From<serenity::utils::Color> for Color {
     fn from(c: serenity::utils::Colour) -> Self {
         Self(c.0)
